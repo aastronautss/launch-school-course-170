0682679c7c5618d4bb207f6c9475fae31b737c5d
Tyler Guillen <tyguillen@gmail.com>
2016-02-28 12:16:44 -0500

    finish book reader search function

---
+++ lesson-2/exercises.md
@@ -0,0 +1,11 @@
+# Course 170, Lesson 2 Exercises
+
+## The Request-Response Cycle
+
+**1.** For a request, the method and the path are the required components. Optional components include headers and a body. Parameters are technically a part of the path, but are optional.
+
+**2.** An HTTP resposne requires status codes. Headers and a body are optional.
+
+**3.** GET requests are typically done when the client wants to retrieve data, and POST requests are done when the client wants to submit data to the server.
+
+**4.** The host component of a URL could be present in an HTTP response, but is not required.
---
+++ lesson-4/book-viewer/book_viewer.rb
@@ -0,0 +1,60 @@
+require "sinatra"
+require "sinatra/reloader"
+require 'tilt/erubis'
+
+require 'pry'
+
+before do
+  @toc = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def in_paragraphs(text)
+    paragraphs = paragraph_array(text)
+    paragraphs.map.with_index do |paragraph, id|
+      "<p id=\"#{id}\">#{paragraph}</p>"
+    end.join
+  end
+
+  def paragraph_array(text)
+    text.split "\n\n"
+  end
+
+  def format_search_result(text, query)
+    text.gsub(/(#{query})/i, '<strong>\1</strong>')
+  end
+end
+
+not_found do
+  redirect ''
+end
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+  erb :home
+end
+
+get '/chapters/:number' do
+  chapter_number = params[:number].to_i
+
+  @chapter_text = File.read("data/chp#{chapter_number}.txt")
+  @title = "Chapter #{chapter_number}: #{@toc[chapter_number - 1]}"
+
+  erb :chapter
+end
+
+get '/search' do
+  if params[:query]
+    @results = @toc.each_with_index.each_with_object([]) do |(chap, index), arr|
+      chap_text = File.read("data/chp#{index + 1}.txt")
+      paragraphs = paragraph_array(chap_text)
+      paragraphs.each_with_index do |paragraph, paragraph_index|
+        if paragraph.downcase.include? params[:query].downcase
+          arr << [chap, index, paragraph, paragraph_index]
+        end
+      end
+    end
+  end
+
+  erb :search
+end
